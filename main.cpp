263c4fc1d30e43716aaf255bf7bc15ab567fca63
Octavian S <95926496+TwYxL@users.noreply.github.com>
2023-03-21 13:23:24 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+// header files
+#include <iostream>
+#include <time.h>
+using namespace std;
+
+
+// main function
+int main()
+{
+    srand(time(0)); // seeding for random numbers
+    string pass;
+
+    // all data
+    string data = "abcdefghijklmnopqrstuvwxyz"
+                  "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                  "0123456789"
+                  "!@#$%^&*()_-+=;~[]{}|,></:?";
+
+                  for(int i = 1; i <= 12; i++)
+                  {
+                      pass = pass + data[rand()%93]; // getting generated
+                  }
+
+                  cout<<"Your password: "<< pass; // displaying the password
+
+                  return 0;
+
+}
